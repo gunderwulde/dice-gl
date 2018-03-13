23db0b3f52afb868ca0745ec5ed8f50c128bc388
Glitch (dice-gl) <none>
2018-03-13 08:52:58 +0000

    🚤🌷 Updated with Glitch

--- Lib/Camera.js
+++ Lib/Camera.js
@@ -3,7 +3,7 @@ function Camera() {
   this.viewProjectionMatrix = new Matrix4();
   this.viewMatrix = new Matrix4();
   this.Position(0,0,0);
-  this.Rotation(0,0,0);
+  this.Rotation(new Quaternion());
   
   gl.clearColor(0.75, 0.75, 0.75, 1.0);
   gl.clearDepth(1.0);
@@ -16,16 +16,11 @@ function Camera() {
 }
 
 Camera.prototype.Position = function (x,y,z) { this.px=x; this.py=y; this.pz=z; this.dirty = true;}
-Camera.prototype.Rotation = function (x,y,z) { this.rx=x* Math.PI / 180; this.ry=y* Math.PI / 180; this.rz=z* Math.PI / 180; this.dirty = true;}
+Camera.prototype.Rotation = function (q) { this.rotation=q; this.dirty = true;}
 
 Camera.prototype.Update = function(){
   if(this.dirty){
-/*
-    this.viewMatrix.rotationEuler(this.rx, this.ry, this.rz);
-    this.viewMatrix.position( this.px, this.py, this.pz);
-*/    
-    var rotationMatrix = new Matrix4();
-	  rotationMatrix.rotationEuler(-this.rx, -this.ry, -this.rz);
+    var rotationMatrix = this.rotation.ToMatrix();
     var positionMatrix = new Matrix4();
     positionMatrix.position( -this.px, -this.py, -this.pz);
     this.viewMatrix.multiply(rotationMatrix, positionMatrix );
---
+++ Lib/Quaternion.js
@@ -0,0 +1,38 @@
+function Quaternion( x, y, z, w ) {
+	this._x = x || 0;
+	this._y = y || 0;
+	this._z = z || 0;
+	this._w = ( w !== undefined ) ? w : 1;
+}
+
+Quaternion.prototype.ToMatrix = function(){
+  var m = new Matrix4();
+  
+  var sqw = this._w*this._w;
+  var sqx = this._x*this._x;
+  var sqy = this._y*this._y;
+  var sqz = this._z*this._z;
+
+    // invs (inverse square length) is only required if quaternion is not already normalised
+  var invs = 1 / (sqx + sqy + sqz + sqw);
+  
+  m.elements[0] = ( sqx - sqy - sqz + sqw)*invs ; // since sqw + sqx + sqy + sqz =1/invs*invs
+  m.elements[5] = (-sqx + sqy - sqz + sqw)*invs ;
+  m.elements[10] = (-sqx - sqy + sqz + sqw)*invs ;
+    
+  var tmp1 = this._x*this._y;
+  var tmp2 = this._z*this._w;
+  m.elements[4] = 2.0 * (tmp1 + tmp2)*invs ; // m10
+  m.elements[1] = 2.0 * (tmp1 - tmp2)*invs ; // m01
+
+  var tmp1 = this._x*this._z;
+  var tmp2 = this._y*this._w;
+  m.elements[8] = 2.0 * (tmp1 - tmp2)*invs ; // m20
+  m.elements[2] = 2.0 * (tmp1 + tmp2)*invs ; // m02
+  var tmp1 = this._y*this._z;
+  var tmp2 = this._x*this._w;
+  m.elements[9] = 2.0 * (tmp1 + tmp2)*invs ; // m21
+  m.elements[6] = 2.0 * (tmp1 - tmp2)*invs ; // m12
+  
+return m;
+}
